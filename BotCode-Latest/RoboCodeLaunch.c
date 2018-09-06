1c8e5a937e9b1a793f52af4b1aa535d7abd85c62
DynamiteFox <tristanntaylor674@gmail.com>
2018-09-06 14:37:44 -0700

    new safe zone code

--- BotCode-Latest/RoboCodeLaunch.c
+++ BotCode-Latest/RoboCodeLaunch.c
@@ -23,7 +23,7 @@
 //Main competition background code...do not modify!
 #include "Vex_Competition_Includes.c"
 
-#include "RoboCode-Latest v0.1.3.c"
+#include "RoboCode-Latest v0.2.0.c"
 
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
