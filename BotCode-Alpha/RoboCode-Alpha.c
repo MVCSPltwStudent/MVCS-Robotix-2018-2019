5921f9c9915547dcdd1f57586fb4d64829b53576
DynamiteFox <tristanntaylor674@gmail.com>
2018-10-11 18:29:02 -0700

    RoboCode-Alpha v0.2.1
    Fixed Safe Zone Code

--- BotCode-Alpha/RoboCode-Alpha.c
+++ BotCode-Alpha/RoboCode-Alpha.c
@@ -1,4 +1,4 @@
-//RoboCode-Alpha v0.2.0
+//RoboCode-Alpha v0.2.1
 
 
 
@@ -31,24 +31,29 @@ Port10: Platform Boost system (?)
 // USE SEPARATE TASKS FOR EVERYTHING. IT ACCOMPLISHES THE SAME THING AS PUTTING EVERYTHING IN MAIN,
 // BUT IT'S MORE ORGANIZED AND HELPS DEBUG
 //-----------------------------------------------IMPORTANT---------------------------------------------
+int FR;
+int FL;
+int BR;
+int BL;
 
-int absv (int a) {
+int mabs (int a) {
 	return a < 0 ? -a : a;
 }
 
 task drivetrain(){ //Drivetrain Task. Joshua's code.
-
 	while(true){
-		if (absv(vexRT[Ch4]) > 15 || absv(vexRT[Ch3]) > 15 || absv(vexRT[Ch1]) > 15) {
-			motor[mFR]=(-vexRT[Ch4]+vexRT[Ch3])-vexRT[Ch1];
-	  	motor[mFL]=(-vexRT[Ch4]-vexRT[Ch3])-vexRT[Ch1];
-	  	motor[mBL]=(vexRT[Ch4]-vexRT[Ch3])-vexRT[Ch1];
-	  	motor[mBR]=(vexRT[Ch4]+vexRT[Ch3])-vexRT[Ch1];
-		}
-	  /*
-	  if(abs(vexRT[Ch4]) < 20 && abs(vexRT[Ch3]) < 20 && abs(vexRT[Ch1]) < 20){
-	    motor[mFR] = motor[mFL] = motor[mBL] = motor[mBR] = 0;
-	  }*/
+		FR=(-vexRT[Ch4]+vexRT[Ch3])-vexRT[Ch1]; //Determines motor speeds. Joshua's Code.
+	  FL=(-vexRT[Ch4]-vexRT[Ch3])-vexRT[Ch1];
+	  BL=(vexRT[Ch4]-vexRT[Ch3])-vexRT[Ch1];
+	  BR=(vexRT[Ch4]+vexRT[Ch3])-vexRT[Ch1];
+	  if(mabs(vexRT[Ch4]) < 20 && mabs(vexRT[Ch3]) < 20 && mabs(vexRT[Ch1]) < 20){ //Checks if joystick is in deadzone
+	    FR = FL = BR = BL = 0;																									//if so, sets motor speeds to 0
+	  }
+	  motor[mFR] = FR;  //Applies Motor Speeds
+	  motor[mFL] = FL;
+	  motor[mBR] = BR;
+	  motor[mBL] = BL;
+	  EndTimeSlice(); //tells task handler is done
 	}
 }
 
@@ -58,10 +63,11 @@ task ballGrabber(){ //Ball intake system code
 		if (vexRT[Btn6U]){ //if 6U pressed...
 			motor[mGRB] = 127; //run intake motor
 	  }else if (vexRT[Btn6D]){ //if 6D Pressed
-	  	motor[mGRB] = -127;
+	  	motor[mGRB] = -127; // reverse
 		} else {
-			motor[mGRB] = 0;
+			motor[mGRB] = 0; //else stop
 		}
+		EndTimeSlice(); //tell task handler done
 	}
 }
 
