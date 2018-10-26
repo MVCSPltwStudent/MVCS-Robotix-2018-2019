d7394076c988f83f501e33d482e3ab6dc95be2a0
David Chen <chenwei2000121@163.com>
2018-10-26 13:19:59 -0700

    Update RoboCode-Alpha.c

--- BotCode-Alpha/RoboCode-Alpha.c
+++ BotCode-Alpha/RoboCode-Alpha.c
@@ -54,18 +54,18 @@ int mabs (int a) {
 
 task drivetrain(){ //Drivetrain Task. Joshua's code.
 	while(true){
-		FR=(-vexRT[Ch4]+vexRT[Ch3])-vexRT[Ch1]; //Determines motor speeds. Joshua's Code.
-		FL=(-vexRT[Ch4]-vexRT[Ch3])-vexRT[Ch1];
-		BL=(vexRT[Ch4]-vexRT[Ch3])-vexRT[Ch1];
-		BR=(vexRT[Ch4]+vexRT[Ch3])-vexRT[Ch1];
-		if(mabs(vexRT[Ch4]) < 20 && mabs(vexRT[Ch3]) < 20 && mabs(vexRT[Ch1]) < 30){ //Checks if joystick is in deadzone
+		FR = (-vexRT[Ch4] + vexRT[Ch3]) - vexRT[Ch1]; //Determines motor speeds. Joshua's Code.
+		FL = (-vexRT[Ch4] - vexRT[Ch3]) - vexRT[Ch1];
+		BL = (vexRT[Ch4] - vexRT[Ch3]) - vexRT[Ch1];
+		BR = (vexRT[Ch4] + vexRT[Ch3]) - vexRT[Ch1];
+		if (mabs(vexRT[Ch4]) < 20 && mabs(vexRT[Ch3]) < 20 && mabs(vexRT[Ch1]) < 30){ //Checks if joystick is in deadzone
 			FR = FL = BR = BL = 0;																									//if so, sets motor speeds to 0
 		}
 		if(sqrt((vexRT[Ch4]^2)+(vexRT[Ch3]^2))<127){
-			FR = FR/2;
-			FL = FL/2;
-			BR = BR/2;
-			BL = BL/2;
+			FR = FR / 2;
+			FL = FL / 2;
+			BR = BR / 2;
+			BL = BL / 2;
 		}
 			motor[mFR] = FR;  //Applies Motor Speeds
 			motor[mFL] = FL;
@@ -79,21 +79,21 @@ task drivetrain(){ //Drivetrain Task. Joshua's code.
 
 task ballGrabber(){ //Ball intake system code
 	while(true){
-		if (vexRT[Btn6U]){ //if 6U pressed...
+		if (vexRT[Btn6U]) { //if 6U pressed...
 			motor[mGRB] = 127; //run intake motor
-			}else if (vexRT[Btn6D]){ //if 6D Pressed
+		} else if (vexRT[Btn6D]) { //if 6D Pressed
 			motor[mGRB] = -127; // reverse
-			} else {
+		} else {
 			motor[mGRB] = 0; //else stop
 		}
 		EndTimeSlice(); //tell task handler done
 	}
 }
-task speedToggle(){
-	while(true){
-		if(vexRT[Btn7U]){
+task speedToggle() {
+	while (true) {
+		if (vexRT[Btn7U]) {
 			halfSpeed = !halfSpeed;
-			while(vexRT[Btn7U]){
+			while (vexRT[Btn7U]) {
 				wait1Msec(1);
 			}
 		}
@@ -101,18 +101,18 @@ task speedToggle(){
 	}
 }
 
-task flywheelToggle(){ //detects button presses to toggle the flywheel
-	while (true){
-		if(vexRT[Btn8D]){ //if toggle button pressed
+task flywheelToggle() { //detects button presses to toggle the flywheel
+	while (true) {
+		if (vexRT[Btn8D]) { //if toggle button pressed
 			flywheelRunning = !flywheelRunning;  //set flywheel running bool to the opposite value
-			while(vexRT[Btn8D]){ //wait until button un-pressed
+			while (vexRT[Btn8D]) { //wait until button un-pressed
 				wait1Msec(1);
 			}
 		}
-		if(flywheelRunning){ // if flywheel set to be on
+		if (flywheelRunning) { // if flywheel set to be on
 			motor[mFRE] = flySpeed + (5*flyModifier); //turn on
 			motor[port9] = flySpeed + (5*flyModifier);
-		}else{
+		} else {
 			motor[mFRE] = 0; //else turn off
 			motor[port9] = 0;
 		}
@@ -141,8 +141,8 @@ task flySpeedAdjuster() {
 		  }
 	}
 	if (vexRT[Btn8R]) {
-		 while(!vexRT[Btn8R]){wait1Msec(1);}
-		 switch(flySpeed){
+		 while (!vexRT[Btn8R]){wait1Msec(1);}
+		 switch (flySpeed) {
 		   case 127:
 		   	flySpeed = lv3;
 		   break;
